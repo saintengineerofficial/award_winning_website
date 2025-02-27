b829fb414026e1ecf0404822ebd4338b00c284f2
caizhihao <caizhihao@suntang.com>
2025-02-27 11:29:00 +0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
-// import Hero from "@/sections/Hero";
-// import About from "@/sections/About";
-// import Navbar from "@/sections/Navbar";
-// import Features from "@/sections/Features";
-// import Story from "@/sections/Story";
-// import Contact from "@/sections/Contact";
+import Hero from "@/sections/Hero";
+import About from "@/sections/About";
+import Navbar from "@/sections/Navbar";
+import Features from "@/sections/Features";
+import Story from "@/sections/Story";
+import Contact from "@/sections/Contact";
 import Footer from "@/sections/Footer";
 export default function Home() {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
       11111
-      {/* <Navbar />
+      <Navbar />
       <Hero />
       <About />
       <Features />
       <Story />
-      <Contact /> */}
+      <Contact />
       <Footer />
     </main>
   );
